ab47aed2d2d1035e55a19d8faa8c1e2d9c126d53
songya <songya@umich.edu>
2019-01-26 17:47:30 -0500

    process command line args to ask for input pdf

--- pdfmanip/__main__.py
+++ pdfmanip/__main__.py
@@ -1,9 +1,16 @@
 """Rotate a pdf from the command line"""
 
-def main():
-    """Top Level Command Line Interface"""
-    print('Hello World!')
+import click        # command line argument processing
+import PyPDF2       # pdf processing
 
+# process command line arguments
+# TODO: add option to rotate left or right
+@click.command()
+@click.argument('input_pdf')
+def main(input_pdf):
+    """Top Level Command Line Interface"""
+
+    print('Hello World!')
 
 if __name__ == "__main__":
     main()
