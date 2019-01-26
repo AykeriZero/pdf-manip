f7872db509460cb36dccdca737766c5f59bff49b
songya <songya@umich.edu>
2019-01-26 18:02:47 -0500

    basic working model, default output, only rotate left

--- pdfmanip/__main__.py
+++ pdfmanip/__main__.py
@@ -10,6 +10,19 @@ import PyPDF2       # pdf processing
 def main(input_pdf):
     """Top Level Command Line Interface"""
 
+    with open(input_pdf, 'rb') as pdf_in:
+        pdf_reader = PyPDF2.PdfFileReader(pdf_in)
+        pdf_writer = PyPDF2.PdfFileWriter()
+
+        for pagenum in range(pdf_reader.numPages):
+            page = pdf_reader.getPage(pagenum)
+            page.rotateClockwise(-90)
+            pdf_writer.addPage(page)
+
+        # input_pdf needs to be open?
+        with open('rotated.pdf', 'wb') as pdf_out:
+            pdf_writer.write(pdf_out)
+
     print('Hello World!')
 
 if __name__ == "__main__":
