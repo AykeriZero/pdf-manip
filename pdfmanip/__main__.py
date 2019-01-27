d3bd34c6b4e853be2b5133cbe6bd3afe4373f773
songya <songya@umich.edu>
2019-01-27 18:00:38 -0500

    added option for rotating left

--- pdfmanip/__main__.py
+++ pdfmanip/__main__.py
@@ -6,8 +6,9 @@ import PyPDF2       # pdf processing
 # process command line arguments
 # TODO: add option to rotate left or right
 @click.command()
+@click.option('-l', '--left', is_flag=True, help='Rotate left')
 @click.argument('input_pdf')
-def main(input_pdf):
+def main(left, linput_pdf):
     """Top Level Command Line Interface"""
 
     with open(input_pdf, 'rb') as pdf_in:
@@ -16,14 +17,18 @@ def main(input_pdf):
 
         for pagenum in range(pdf_reader.numPages):
             page = pdf_reader.getPage(pagenum)
-            page.rotateClockwise(-90)
+            
+            if (left):
+                page.rotateClockwise(90)
+            else:
+                page.rotateCounterClockwise(90)
+
             pdf_writer.addPage(page)
 
         # input_pdf needs to be open?
         with open('rotated.pdf', 'wb') as pdf_out:
             pdf_writer.write(pdf_out)
 
-    print('Hello World!')
 
 if __name__ == "__main__":
     main()
